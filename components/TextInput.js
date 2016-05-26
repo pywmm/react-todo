8df544df7232df23aa6e5ec9d98bc7ead1eef7fe
peilin <hzpeilin@corp.netease.com>
2016-05-27 00:55:46 +0800

    add file

---
+++ components/TextInput.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class TextInput extends Component {
+
+/*	getInitialState() {
+		return {
+			inputText:'getInitialState'
+		}
+	}*/
+
+	constructor (props, context) {
+		super(props, context)
+		this.state = {
+			inputText: 'initial text'
+		}
+	}
+
+	render() {
+		return (
+			<input 
+				type="text" 
+				placeholder = "This is going to be text"
+				value = {this.state.inputText}
+			/>
+		)
+	}
+}
+
+export default TextInput
